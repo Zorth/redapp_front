bdfdd0e4fdfe6feed7b1854e05377492b9a03727
Jasper Goens <jasper_goens@hotmail.com>
2024-05-23 02:27:27 +0200

    API mongoDB dependencies

---
+++ redapp_api/src/error.rs
@@ -0,0 +1,13 @@
+
+
+#[derive(thiserror:Error, Debug)]
+pub enum Error {
+    #[error("Value not of type '{0}'")]
+    XValueNotOfType(&'static str),
+
+    #[error(transparent)]
+    Surreal(#[from] surrealdb::Error),
+
+    #[error(transparent)]
+    IO(#[from] std::io::Error),
+}
