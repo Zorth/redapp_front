dfb2918fd467462963a9731fc3b53d8207bdd525
Jasper Goens <jasper_goens@hotmail.com>
2024-05-23 02:43:56 +0200

    setup basic MongoDB setup env variables

--- redapp_api/src/main.rs
+++ redapp_api/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use mongodb::{ 
+	bson::{Document, doc},
+	Client,
+	Collection 
+};
+
+#[tokio::main]
+async fn main() -> mongodb::error::Result<()> {
+    // Replace the placeholder with your Atlas connection string
+    let uri;  
+    match std::env::var("CORIOLISDB") {
+        Ok(val) => uri = val,
+        Err(e) => {
+            println!("Env var not found: {e}");
+            std::process::exit(404);
+        },
+    }
+
+    // Create a new client and connect to the server
+    let client = Client::with_uri_str(uri).await?;
+
+    // Get a handle on the movies collection
+    let database = client.database("sample_mflix");
+    let my_coll: Collection<Document> = database.collection("movies");
+
+    // Find a movie based on the title value
+    let my_movie = my_coll.find_one(doc! { "title": "The Perils of Pauline" }, None).await?;
+
+    // Print the document
+    println!("Found a movie:\n{:#?}", my_movie);
+    Ok(())
 }
+
