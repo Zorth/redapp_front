c91dc54cecdabd1b0ad8d2cb45f5a6f6c6e2f6f1
Jasper Goens <jasper_goens@hotmail.com>
2024-05-23 20:45:40 +0200

    api axum init

--- redapp_api/src/main.rs
+++ redapp_api/src/main.rs
@@ -1,11 +1,28 @@
+use axum::{
+    routing::{get, post},
+    http::StatusCode,
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
 use mongodb::{ 
-	bson::{Document, doc},
-	Client,
-	Collection 
+    bson::{Document, doc},
+    Client,
+    Collection 
 };
 
 #[tokio::main]
 async fn main() -> mongodb::error::Result<()> {
+
+    let app: Router = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root));
+    // `POST /users` goes to `create_user`
+    // .route("/users", post(create_user));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+
     // Replace the placeholder with your Atlas connection string
     let uri;  
     match std::env::var("CORIOLISDB") {
@@ -30,4 +47,10 @@ async fn main() -> mongodb::error::Result<()> {
     println!("Found N0rt0n:\n{:#?}", my_movie);
     Ok(())
 }
+
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
 
